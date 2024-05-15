03051c891b7cda486ae771f5d55ce9cbf42a0423
Maithili-Londhe <londhemaithili99@gmail.com>
2024-05-15 17:28:55 +0530

    my second commit

---
+++ src/com/javaClassAndObject/Student.java
@@ -0,0 +1,16 @@
+package com.javaClassAndObject;
+
+public class Student {
+
+	//fields
+	private int studentId;
+	private String studentName;
+
+	public Student() 
+	{
+		System.out.println("Hello constructor");
+	}
+
+
+
+}
